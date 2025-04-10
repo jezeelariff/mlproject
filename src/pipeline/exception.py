05234455006c8c10ddd06c25f65e647bccf895b5
jezeelariff <jazeelariff@gmail.com>
2025-04-10 12:52:11 +0530

    Data Ingestion

--- src/pipeline/exception.py
+++ src/pipeline/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.pipeline.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_, exc_tb = error_detail.exc_info() # which file the error has occured, on which line the error has occured
@@ -17,10 +17,10 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-# if __name__ == "__main__":
+if __name__ == "__main__":
 
-#     try:
-#             a = 1/0
-#     except Exception as e:
-#         logging.info("Division by zero")
-#         raise CustomException(e,sys)
+    try:
+            a = 1/0
+    except Exception as e:
+        logging.info("Division by zero")
+        raise CustomException(e,sys)
